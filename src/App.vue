c2c26534ffd4ff7c8158692db98c5ddde5de8017
serii <seriiburduja@gmail.com>
2020-10-16 11:08:36 +0300

    vuleLink

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-goals></app-goals>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Goals from './components/Goals.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appGoals: Goals
   }
 }
 </script>
---
+++ src/components/Goals.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ courseGoal }}</h2>
+    <p>Vue js
+      <a target="_blank" :href="vueLink">link</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      courseGoal: "Finish the course and learn Vue",
+      vueLink: 'http://vuejs.org'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
