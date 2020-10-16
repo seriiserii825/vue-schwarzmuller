1f2332a3fdde22ca75990493ac9b45a8517bec16
serii <seriiburduja@gmail.com>
2020-10-16 22:10:00 +0300

    styling

---
+++ src/components/Box.vue
@@ -0,0 +1,28 @@
+<template>
+    <div
+        @click="selectBox"
+        :class="['demo' ,{'active' : boxSelected}]"
+    >{{ boxName.title }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['boxName'],
+    data() {
+        return {
+            boxSelected: false
+        }
+    },
+    methods: {
+        selectBox() {
+            console.log('clicked');
+            this.boxSelected = !this.boxSelected
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/YourName.vue
+++ src/components/YourName.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="assignment">
-        <h2>Events in action</h2>
-        <button @click="changeCounter(5)">Add 5</button>
-        <button @click="changeCounter(1)">Add 1</button>
-        <p>Result: {{ result }}</p>
+        <header>
+            <h1>Vue Dynamic Styling</h1>
+        </header>
+        <section id="styling">
+            <div class="demo"></div>
+            <div class="demo"></div>
+            <div class="demo"></div>
+        </section>
     </div>
 </template>
 
@@ -11,84 +15,37 @@
 export default {
     data() {
         return {
-            counter: 0,
-            result: 0
-        }
-    },
-    methods: {
-        changeCounter(value) {
-            this.counter += value
-        }
-    },
-    watch: {
-        counter() {
-            const result = this.counter
-
-            setTimeout(() => {
-                if (result === this.counter) {
-                    this.counter = 0
-                }
-            }, 5000)
-
-            if (this.counter < 37) {
-                this.result = 'Not there yet'
-            } else {
-                this.result = 'Too much'
-            }
+            demos: [
+                {id: 1, title: "demo1"},
+                {id: 2, title: "demo2"},
+                {id: 3, title: "demo3"}
+            ]
         }
     }
 }
 </script>
 
 <style scoped>
-.assignment {
-    margin: 280px auto;
-    max-width: 600px;
+header {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 3rem;
+    border-radius: 10px;
+    padding: 1rem;
+    background-color: #4fc08d;
+    color: white;
     text-align: center;
 }
-section {
+#styling {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     margin: 3rem;
     border-radius: 10px;
     padding: 1rem;
     text-align: center;
 }
-h2 {
-    font-size: 2rem;
-    border-bottom: 4px solid #ccc;
-    color: #970076;
-    margin: 0 0 1rem 0;
-}
-p {
-    font-size: 1.25rem;
-    font-weight: bold;
-    background-color: #970076;
-    padding: 0.5rem;
-    color: white;
-    border-radius: 25px;
-}
-input {
-    font: inherit;
-    border: 1px solid #ccc;
-}
-input:focus {
-    outline: none;
-    border-color: #1b995e;
-    background-color: #d7fdeb;
-}
-button {
-    font: inherit;
-    cursor: pointer;
-    border: 1px solid #f07;
-    background-color: #f07;
-    color: white;
-    padding: 0.05rem 1rem;
-    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-button:hover,
-button:active {
-    background-color: #ec3169;
-    border-color: #ec3169;
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+.demo {
+    width: calc(100% - 32px);
+    height: 100px;
+    margin: 16px;
+    border: 2px dashed #ccc;
 }
 </style>
