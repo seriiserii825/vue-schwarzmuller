640ab53dbc15763aa9010146cc3929e801c143d7
serii <seriiburduja@gmail.com>
2020-10-18 19:47:41 +0300

    add content from zip

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
     <div id="app">
-        <app-monster-slayer/>
+        <header>
+            <h1>FriendList</h1>
+        </header>
+        <section id="app">
+            <ul>
+                <li>
+                    <h2>Manuel Lorenz</h2>
+                    <button>Show Details</button>
+                    <ul>
+                        <li><strong>Phone:</strong> 01234 5678 991</li>
+                        <li><strong>Email:</strong> manuel@localhost.com</li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Julie Jones</h2>
+                    <button>Show Details</button>
+                    <ul>
+                        <li><strong>Phone:</strong> 09876 543 221</li>
+                        <li><strong>Email:</strong> julie@localhost.com</li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
     </div>
 </template>
 
 <script>
-import MonsterSlayer from './components/MonsterSlayer';
 
 export default {
     name: 'App',
-    components: {
-        appMonsterSlayer: MonsterSlayer
-    }
 }
 </script>
 
@@ -36,5 +54,51 @@ ul {
     margin: 0;
     padding: 0;
     list-style-type: none;
+}
+header {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 3rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    background-color: #58004d;
+    color: white;
+    text-align: center;
+    width: 90%;
+    max-width: 40rem;
+}
+#app ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+#app li {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 1rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: center;
+    width: 90%;
+    max-width: 40rem;
+}
+#app h2 {
+    font-size: 2rem;
+    border-bottom: 4px solid #ccc;
+    color: #58004d;
+    margin: 0 0 1rem 0;
+}
+#app button {
+    font: inherit;
+    cursor: pointer;
+    border: 1px solid #f07;
+    background-color: #f07;
+    color: white;
+    padding: 0.05rem 1rem;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+#app button:hover,
+#app button:active {
+    background-color: #ec3169;
+    border-color: #ec3169;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
 </style>
