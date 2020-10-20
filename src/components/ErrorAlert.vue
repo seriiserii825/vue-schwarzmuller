b3e97fd88592ff34e306572a089d337c9b1f921b
serii <seriiburduja@gmail.com>
2020-10-20 19:58:34 +0300

    error message

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="error-alert">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.error-alert {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+    width: 300px;
+    border: 1px solid #ccc;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, .4);
+    background-color: white;
+}
+</style>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text">
-  </div>
+    <div>
+        <h2>Manage Goals</h2>
+        <input type="text" v-model="inputValue">
+        <button @click="sendMessage">Send message</button>
+
+        <error-alert v-if="inputIsInvalid">
+            <h2>Have an error</h2>
+            <p>Input don't must to be empty!!!!</p>
+            <button @click="changeInputIsInvalid">Ok</button>
+        </error-alert>
+    </div>
 </template>
 
 <script>
-export default {}
+import ErrorAlert from "@/components/ErrorAlert";
+
+export default {
+    data() {
+        return {
+            inputIsInvalid: false,
+            inputValue: ''
+        }
+    },
+    methods: {
+        sendMessage() {
+            if (this.inputValue === '') {
+                this.showAlert()
+            }
+        },
+        showAlert() {
+            this.inputIsInvalid = true
+        },
+        changeInputIsInvalid() {
+            this.inputIsInvalid = false
+        }
+    },
+    components: {
+        ErrorAlert
+    }
+}
 </script>
 
 <style scoped>
